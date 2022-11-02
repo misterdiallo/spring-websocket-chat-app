6e81cbb174a427f3216e8263810f18babdcab298
Mister Diallo <77695206+misterdiallo@users.noreply.github.com>
2022-11-02 21:34:21 +0800

    Initial spring (websocket, lombok, and devtools)

---
+++ src/main/java/com/example/spring_chat_app/SpringChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_chat_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChatAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringChatAppApplication.class, args);
+    }
+
+}
